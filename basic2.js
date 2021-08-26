9f83bd8d5cfa6562e208c76bee5b0d9c2897b3d0
lamiya <lamiyalami677@gmail.com>
2021-08-26 12:29:08 +0530

    forth commit

--- basic2.js
+++ basic2.js
@@ -51,3 +51,102 @@ for (var i = 0; i < arr1.length; i++) {
 }
 document.write(item+"is the most freq item");
 alert(item + " ( " + mf + " times ) ");
+document.write("<br>");
+
+
+var str=prompt("enter a string");
+var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+var result = [];
+  
+  for(var x=0; x<str.length; x++)
+  {
+    if(UPPER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toUpperCase());
+    }
+    else 
+    {
+      result.push(str[x]);
+    }
+  }
+document.write(result.join(''));
+document.write("<br>");
+
+
+var num1 =parseInt(prompt('enter the number'));
+for( var i=0;i<16;i++){
+  if(num1%2===0){
+    document.write("number is even");
+  }
+  else{
+    document.write("number is odd");
+  }
+  break;
+}
+document.write("<br>");
+
+var length = 3;
+var myString =prompt("enter your string");
+var myTruncatedString = myString.substring(0,length);
+
+document.write("truncated string is "+ myTruncatedString);
+document.write("<br>");
+
+var num2 =parseInt(prompt('enter the  first number'));
+var num3 =parseInt(prompt('enter the  second number'));
+ if(num2>num3){
+  document.write("largest num is" + num2);
+ }
+ else{
+  document.write(" largest num is"+ num3);
+ }
+ 
+ document.write("<br>");
+
+
+
+const string = prompt('Enter an array: ');
+
+
+const words = string.split(' ');
+
+
+const z=words.sort();
+document.write(" sorted array are : "+ z);
+
+
+
+for (const element of words) {
+  console.log(element);
+}
+document.write("<br>");
+
+var str="javascript";
+for( var i=0;i<=9;i++)
+{
+    for( var j=0;j<=i;j++)  
+    {
+        document.write("&nbsp"+ str[j]);
+    }
+    document.write("\n");
+    document.write("<br>");
+}
+document.write("<br>");
+
+
+
+
+function sum_sq(array) {
+  var sum = 0, 
+      i = array.length;
+  while (i--) 
+   sum += Math.pow(array[i], 2);
+  return sum;
+}
+ 
+document.write(sum_sq([0,1,2,3,4])); 
